f94ceed5c3d66d2170898059b426f1dd973ddf7e
Dhruv Aeron <dhruv.aeron3@gmail.com>
2023-02-28 13:30:09 +0530

    first commit

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyDzPi8-BGNYljXIr843CvTj6Z1nf-uShuw",
+  authDomain: "netflix-clone-2c4e5.firebaseapp.com",
+  projectId: "netflix-clone-2c4e5",
+  storageBucket: "netflix-clone-2c4e5.appspot.com",
+  messagingSenderId: "161059185258",
+  appId: "1:161059185258:web:1ca29ff0dd8da572522013",
+};
